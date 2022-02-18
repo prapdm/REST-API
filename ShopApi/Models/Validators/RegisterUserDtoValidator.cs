5e3e94de3ba2a71473105d6ec3627e73440fff99
Paweł <prapdm@users.noreply.github.com>
2022-02-18 19:36:38 +0100

    chore: custom validator for register users added

---
+++ ShopApi/Models/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using ShopApi.Entities;
+using System;
+using System.Linq;
+
+namespace ShopApi.Models.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator(ShopDBContext dBContext)
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
+            RuleFor(x => x.ConfirmPassword).NotEmpty().Equal(e => e.Password);
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Surname).NotEmpty();
+            RuleFor(x => x.Email)
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dBContext.Users.Any(u => u.Email == value);
+                    if (emailInUse)
+                    {
+                        context.AddFailure("Email", "That email is taken");
+                    }
+
+                });
+        }
+
+    }
+}
