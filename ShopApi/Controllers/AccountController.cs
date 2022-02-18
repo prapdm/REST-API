9d907f63efb7e75d700edf8e494598a783aba568
Paweł <prapdm@users.noreply.github.com>
2022-02-18 19:35:14 +0100

    chore: AccountController.cs added

---
+++ ShopApi/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShopApi.Models;
+using ShopApi.Services;
+
+namespace ShopApi.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]  
+        public ActionResult RegisterUser([FromBody] RegisterUserDto dto)
+        {
+            _accountService.RegisterUser(dto);
+            return Ok("The user has been added, but need to activate by administrator.");
+        }
+
+        [HttpPost("activate")]
+        [Authorize(Roles = "Administrator")]
+        public ActionResult ActivateUser([FromBody] ActivateUserDto dto)
+        {
+            _accountService.ActivateUser(dto);
+            return Ok($"Status of user {dto.Email} has beed set to {dto.IsActive}");
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] LoginDto dto)
+        {
+            string token = _accountService.GenerateJwt(dto);
+            return Ok(token);
+        }
+
+    }
+}
