da0607ef39b6872a0f31732ec97e640ced18e00c
Paweł <prapdm@users.noreply.github.com>
2022-02-18 19:41:13 +0100

    chore: Mapping profile for Automaper added

---
+++ ShopApi/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ShopApi.Entities;
+using ShopApi.Models;
+
+namespace ShopApi
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Shop, ShopDto>()
+                .ForMember(m => m.City, c => c.MapFrom(s => s.Address.City))
+                .ForMember(m => m.Street, c => c.MapFrom(s => s.Address.Street))
+                .ForMember(m => m.PostalCode, c => c.MapFrom(s => s.Address.PostalCode));
+        }
+    }
+}
