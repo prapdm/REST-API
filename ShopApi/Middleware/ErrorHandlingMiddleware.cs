fd1c385478f5e49bf9f510f3bd99df8583ad9085
Paweł <prapdm@users.noreply.github.com>
2022-02-18 19:35:53 +0100

    chore: Middleware added

---
+++ ShopApi/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using ShopApi.Exeptions;
+using System;
+using System.Threading.Tasks;
+
+namespace ShopApi.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (ForbidExeption forbidExeption)
+            {
+                context.Response.StatusCode = 403;
+
+            }
+            catch (BadRequestException badRequestExeption)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestExeption.Message);
+            }
+            catch (NotFoundExeption notFoundExeption)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFoundExeption.Message);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong");
+            }
+        }
+    }
+}
---
+++ ShopApi/Middleware/RequestTimeMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+
+namespace ShopApi.Middleware
+{
+    public class RequestTimeMiddleware : IMiddleware
+    {
+        private Stopwatch _stopwatch;
+        private readonly ILogger<RequestTimeMiddleware> _logger;
+
+        public RequestTimeMiddleware(ILogger<RequestTimeMiddleware> logger)
+        {
+            _stopwatch = new Stopwatch();
+            _logger = logger;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            _stopwatch.Start();
+            await next.Invoke(context);
+            _stopwatch.Stop();
+
+            var elipsedMiliseconds = _stopwatch.ElapsedMilliseconds;
+            if (elipsedMiliseconds / 1000 > 4)
+            {
+
+                _logger.LogInformation($"Request [{context.Request.Method}] at {context.Request.Path} took {elipsedMiliseconds} ms");
+            }
+
+        }
+
+
+    }
+}
