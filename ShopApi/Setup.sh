ff355e72c4b6448fb5b9b43ead0e2e4c9ed967f6
Paweł <prapdm@users.noreply.github.com>
2022-02-23 20:25:09 +0100

    chore: add project files

---
+++ ShopApi/Setup.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+set -e
+export  ASPNETCORE_ENVIRONMENT=Docker
+
+dotnet dev-certs https --trust
+
+until /root/.dotnet/tools/dotnet-ef database update ; do
+>&2 echo "SQL Server is starting up"
+sleep 1
+done
+
+>&2 echo "SQL Server is up - executing command"
+
+
+exec dotnet run --launch-profile "Docker-Profile" 
