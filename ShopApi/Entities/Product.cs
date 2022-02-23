449b952586d14b2138771efb43cc8cc7df74f7c2
Paweł <prapdm@users.noreply.github.com>
2022-02-23 20:20:36 +0100

    chore: add Image

--- ShopApi/Entities/Product.cs
+++ ShopApi/Entities/Product.cs
@@ -10,6 +10,7 @@ namespace ShopApi.Entities
         public decimal  Price { get; set; }
         public int ShopId { get; set; }
         public string Category { get; set; }
+        public string ImageUrl { get; set; }
 
         public virtual Shop Shop { get; set; }
         public DateTime CreatedAt { get; set; }
