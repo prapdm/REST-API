0abe480975604cda679c8ea53915abd82e2aaaa5
Paweł <prapdm@users.noreply.github.com>
2022-02-23 20:25:45 +0100

    chore: new mapings profile

--- ShopApi/MappingProfile.cs
+++ ShopApi/MappingProfile.cs
@@ -8,10 +8,27 @@ namespace ShopApi
     {
         public MappingProfile()
         {
+            //CreateMap<From, To>()
+
             CreateMap<Shop, ShopDto>()
                 .ForMember(m => m.City, c => c.MapFrom(s => s.Address.City))
                 .ForMember(m => m.Street, c => c.MapFrom(s => s.Address.Street))
                 .ForMember(m => m.PostalCode, c => c.MapFrom(s => s.Address.PostalCode));
+
+            CreateMap<CreateShopDto, Shop>()
+            .ForMember(r => r.Address,
+                c => c.MapFrom(dto => new Address()
+                { City = dto.City, PostalCode = dto.PostalCode, Street = dto.Street }));
+
+            CreateMap<UpdateShopDto, Shop>()
+            .ForMember(r => r.Address,
+                c => c.MapFrom(dto => new Address()
+                { City = dto.City, PostalCode = dto.PostalCode, Street = dto.Street }));
+
+            CreateMap<CreateUpdateProductDto, Product>()
+                 .ForMember(m => m.ImageUrl, c => c.MapFrom(s => s.file.FileName));
+            CreateMap<Product, ProductDto>();
+                 
         }
     }
 }
